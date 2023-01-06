7295c3951e18e92c25da8957ac5983891b2a1bed
yusupovna1403 <yusupovna2003girl@gmail.com>
2023-01-06 11:33:07 +0500

    task02

--- task02.py
+++ task02.py
@@ -1,5 +1,5 @@
 
-def count_digits(string: str) -> int:
+def count_digits(s):
     """
     Count the number of digits in a string of fixed length.
     args:
@@ -7,5 +7,25 @@ def count_digits(string: str) -> int:
     returns:
         number of digits in the string
     """
+    sum = 0
+    idx = 0
+    if s[idx] >= '0' and s[idx] <= '9':
+        sum+=1
+    idx+=1
+    if s[idx] >= '0' and s[idx] <= '9':
+        sum+=1
+    idx+=1
+    if s[idx] >= '0' and s[idx] <= '9':
+        sum+=1
+    idx+=1
+    if s[idx] >= '0' and s[idx] <= '9':
+        sum+=1
+    idx+=1
+    if s[idx] >= '0' and s[idx] <= '9':
+        sum+=1
+    idx+=1
+    return sum
+    
+print(count_digits("a1b23c"))
 
     # Your code goes here
